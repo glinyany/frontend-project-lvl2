abaa04230ebd75d2d9f8d142e9efcc9110f71fbd
vladyslav hlynianyi <glinyany.lie@gmail.com>
2022-06-17 01:47:48 +0500

    finish step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,9 @@ const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.8.0');
+  .version('0.8.0')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format <type>', 'output format');
  
 program.parse();
