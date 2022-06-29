23510e112791bbe57bbe58d399ed8b5aec1cdae7
vladyslav hlynianyi <glinyany.lie@gmail.com>
2022-06-30 00:30:28 +0500

    fixing issues

--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
 import path from 'path';
 import fs from 'fs';
 import parse from './parsers.js';
-import readFile from './utils.js';
 import buildTree from './differenceHandler.js';
 import getFormat from './formatters/index.js';
 
-// export default (filepath1, filepath2, format = 'stylish') => {
-//   const ext1 = path.extname(filepath1);
-//   const ext2 = path.extname(filepath2);
-
-//   const data1 = parse(readFile(filepath1), ext1);
-//   const data2 = parse(readFile(filepath2), ext2);
-
-//   const differenceObject = buildTree(data1, data2);
-
-//   return getFormat(differenceObject, format);
-// };
 const genDiff = (path1, path2, format = 'stylish') => {
   const ext1 = path.extname(path1);
   const ext2 = path.extname(path2);
@@ -29,3 +17,15 @@ const genDiff = (path1, path2, format = 'stylish') => {
 };
 
 export default genDiff;
+
+// export default (filepath1, filepath2, format = 'stylish') => {
+//   const ext1 = path.extname(filepath1);
+//   const ext2 = path.extname(filepath2);
+
+//   const data1 = parse(readFile(filepath1), ext1);
+//   const data2 = parse(readFile(filepath2), ext2);
+
+//   const differenceObject = buildTree(data1, data2);
+
+//   return getFormat(differenceObject, format);
+// };
