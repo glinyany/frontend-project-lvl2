b0cab885fb33a38c9e05c012c29834f66be0b112
vladyslav hlynianyi <glinyany.lie@gmail.com>
2022-06-30 00:26:45 +0500

    trying to fix path issue

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import path from 'path';
+import fs from 'fs';
 import parse from './parsers.js';
 import readFile from './utils.js';
 import buildTree from './differenceHandler.js';
 import getFormat from './formatters/index.js';
 
-export default (filepath1, filepath2, format = 'stylish') => {
-  const ext1 = path.extname(filepath1);
-  const ext2 = path.extname(filepath2);
+// export default (filepath1, filepath2, format = 'stylish') => {
+//   const ext1 = path.extname(filepath1);
+//   const ext2 = path.extname(filepath2);
 
-  const data1 = parse(readFile(filepath1), ext1);
-  const data2 = parse(readFile(filepath2), ext2);
+//   const data1 = parse(readFile(filepath1), ext1);
+//   const data2 = parse(readFile(filepath2), ext2);
 
-  const differenceObject = buildTree(data1, data2);
-  const styled = getFormat(differenceObject, format);
-  return styled;
+//   const differenceObject = buildTree(data1, data2);
+
+//   return getFormat(differenceObject, format);
+// };
+const genDiff = (path1, path2, format = 'stylish') => {
+  const ext1 = path.extname(path1);
+  const ext2 = path.extname(path2);
+
+  const filePath1 = path.resolve('__fixtures__', path1);
+  const fileObj1 = parse(fs.readFileSync(filePath1, 'utf-8'), ext1);
+  const filePath2 = path.resolve('__fixtures__', path2);
+  const fileObj2 = parse(fs.readFileSync(filePath2, 'utf-8'), ext2);
+  const result = buildTree(fileObj1, fileObj2);
+  return getFormat(result, format);
 };
+
+export default genDiff;
--- src/utils.js
+++ src/utils.js
@@ -4,7 +4,6 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 export default (filename) => readFileSync(getFixturePath(filename), 'utf-8');
