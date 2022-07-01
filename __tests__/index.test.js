f226a1864d1c6238a85ed83f3257afe9a6368cbb
vladyslav hlynianyi <glinyany.lie@gmail.com>
2022-07-02 04:31:51 +0500

    delete utils.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,14 @@
 import { expect, test, describe } from '@jest/globals';
+import path from 'path';
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
 import genDiff from '../index.js';
-import readFile from '../src/utils.js';
+// import readFile from '../src/utils.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
 const cases = [
   ['nestedFile1.yml', 'nestedFile2.yml', 'stylish', 'right-nestedJson.txt'],
--- src/utils.js
+++
@@ -1,9 +0,0 @@
-import path from 'path';
-import { readFileSync } from 'fs';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-
-export default (filename) => readFileSync(getFixturePath(filename), 'utf-8');
